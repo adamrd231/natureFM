aa5608a71c9932434392ed0d3d29092fe8c1d439
adamrd231 <adam.rdconcepts@gmail.com>
2021-08-12 22:35:46 -0400

    Initial Commit

---
+++ natureFM/natureFMApp.swift
@@ -0,0 +1,17 @@
+//
+//  natureFMApp.swift
+//  natureFM
+//
+//  Created by Adam Reed on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct natureFMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
