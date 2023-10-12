d8318f2b7f6860a4d736b3d295b81e4f83f4c99f
Adam Reed <adam.rdconcepts@gmail.com>
2023-10-12 19:53:56 -0400

    Remove debug print statements.

--- natureFM/Services/SongDownloadDataService.swift
+++ natureFM/Services/SongDownloadDataService.swift
@@ -14,7 +14,6 @@ class SongDataDownloadService {
     func getSound(sound: SoundsModel) {
         let soundName = sound.name
         if let savedSoundPath = fileManager.getSoundURL(soundName: soundName, folderName: folderName) {
-            print("Saved sound path: \(savedSoundPath)")
             let soundData = fileManager.getSound(url: savedSoundPath, soundName: soundName, folderName: folderName)
             downloadedSound = soundData
         } else {
@@ -23,18 +22,15 @@ class SongDataDownloadService {
     }
     
     private func downloadSound(_ sound: SoundsModel) {
-        print("Downloading sound")
         let soundName = sound.name
         guard let url = URL(string: sound.audioFileLink) else { return }
         
         songSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> Data? in
-                print("download song tryMap")
                 return data
             })
     
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedData) in
-                print("download song sink")
                 guard let self = self, let downloadedData = returnedData else { return }
 
                 self.downloadedSound = downloadedData
@@ -43,19 +39,12 @@ class SongDataDownloadService {
                 // Update this to not save if playing from main screen
                 self.fileManager.saveSound(soundData: downloadedData, soundName: soundName, folderName: self.folderName)
             })
-          
-        do {
-            print("download song Do this")
+
             URLSession.shared.dataTask(with: url) { (data, response, error) in
-         
+                if let e = error {
+                    print("Error converting url into data: \(e)")
+                }
                 self.downloadedSound = data
             }
-//            let fileData = try Data(contentsOf: url)
- 
-        } catch {
-            print("Error converting url into data")
-        }
     }
-    
-
 }
