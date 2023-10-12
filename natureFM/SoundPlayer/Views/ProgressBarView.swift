e0c768d487a899121fcbe0aae7fb3a60abda3b75
Adam Reed <adam.rdconcepts@gmail.com>
2023-10-12 18:17:43 -0400

    Get layout for playlist on soundplayerview.

---
+++ natureFM/SoundPlayer/Views/ProgressBarView.swift
@@ -0,0 +1,74 @@
+//
+//  ProgressBarView.swift
+//  natureFM
+//
+//  Created by Adam Reed on 10/12/23.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    @EnvironmentObject var homeVM: HomeViewModel
+    @State var wasPlaying: Bool = false
+    @State var isDragging: Bool = false
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { isChanging in
+                if let player = homeVM.audioPlayer {
+                    if player.isPlaying {
+                        homeVM.stopPlayer()
+                        wasPlaying = true
+                    }
+                    
+                }
+            }
+            .onEnded { isDragging in
+                if let player = homeVM.audioPlayer {
+                    let percentageToGoTo = isDragging.location.x / UIScreen.main.bounds.width * 0.9
+                    player.currentTime = percentageToGoTo * player.duration
+                    if wasPlaying {
+                        homeVM.startPlayer()
+                        wasPlaying = false
+                    }
+                }
+            }
+    }
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(Color.theme.backgroundColor)
+                    .frame(width: UIScreen.main.bounds.width * 0.9)
+                Rectangle()
+                    .foregroundColor(Color.theme.customBlue)
+                    .frame(width: ((UIScreen.main.bounds.width * 0.9) * homeVM.percentagePlayed))
+                    .gesture(drag)
+            }
+            .frame(height: 5)
+            .cornerRadius(5)
+            .padding(.horizontal)
+            .overlay(alignment: .leading) {
+                Circle()
+                    .foregroundColor(Color.theme.customBlue)
+                    .frame(width: 10, height: 10)
+                    .offset(x: (UIScreen.main.bounds.width * 0.9) * homeVM.percentagePlayed + 10)
+            }
+            
+            HStack {
+                ClockDisplayView(time: Int(homeVM.currentTime), font: .caption)
+                Spacer()
+                ClockDisplayView(time: Int(homeVM.audioPlayer?.duration ?? 0), font: .caption)
+            }
+            .padding(.horizontal)
+            .font(.caption)
+        }
+    }
+}
+
+struct ProgressBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBarView()
+    }
+}
--- natureFM/SoundPlayer/Views/SoundPlayerView.swift
+++ natureFM/SoundPlayer/Views/SoundPlayerView.swift
@@ -5,21 +5,73 @@ struct SoundPlayerView: View {
     @EnvironmentObject var homeVM: HomeViewModel
 
     var body: some View {
-        VStack {
+        VStack(spacing: 25) {
             if let sound = homeVM.sound {
                 SoundImageView(sound: sound)
             }
            
-            Text(homeVM.sound?.name ?? "N/A")
-                .font(.title2)
-                .fontWeight(.heavy)
+            VStack {
+                Text(homeVM.sound?.name ?? "N/A")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.theme.titleColor)
+                HStack {
+                    Text(homeVM.sound?.categoryName ?? "n/a")
+                    Text("|")
+                    Text(homeVM.sound?.locationName ?? "n/a")
+                }
+                .font(.caption)
+                .fontWeight(.medium)
                 .foregroundColor(Color.theme.titleColor)
+            }
             
-            ProgressBarView()
-                .environmentObject(homeVM)
-          
-            AudioControlButtonsView()
-                .environmentObject(homeVM)
+            VStack {
+                if homeVM.portfolioSounds.count > 0 {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Up next")
+                                .fontWeight(.medium)
+                            Spacer()
+                            Button {
+                                homeVM.isRepeating.toggle()
+                            } label: {
+                                Image(systemName: homeVM.isRepeating ? "repeat.circle.fill" : "repeat")
+                            }
+                            Button {
+                                homeVM.isShuffling.toggle()
+                            } label: {
+                                Image(systemName: homeVM.isShuffling ? "shuffle.circle.fill" : "shuffle")
+                            }
+                        }
+                        .padding(.horizontal)
+                        
+                        List {
+                            ForEach(homeVM.portfolioSounds, id: \.id) { sound in
+                                HStack(spacing: 10) {
+                                    SoundImageView(sound: sound)
+                                        .frame(width: 30, height: 30)
+                                        .clipped()
+                                    Text(sound.name)
+                                    Spacer()
+                                }
+                                .onTapGesture {
+                                    homeVM.stopPlayer()
+                                    homeVM.sound = sound
+                                }
+                            }
+                        }
+                        .listStyle(.plain)
+                        
+                    }
+                    
+                    
+                    AudioControlButtonsView()
+                        .environmentObject(homeVM)
+                    ProgressBarView()
+                      .environmentObject(homeVM)
+                      .padding(.bottom)
+                }
+            }
         }
     }
     
