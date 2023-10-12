0f5f5e335473ee5af960039000fd8efd54f75f3a
Adam Reed <adam.rdconcepts@gmail.com>
2023-10-11 23:55:28 -0400

    Add current progress circle to progress bar

--- natureFM/SoundPlayer/Views/SoundPlayerView.swift
+++ natureFM/SoundPlayer/Views/SoundPlayerView.swift
@@ -4,6 +4,34 @@ import AVKit
 struct SoundPlayerView: View {
     @EnvironmentObject var homeVM: HomeViewModel
     private let buttonSize: CGFloat = 30
+    @State var wasPlaying: Bool = false
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { isChanging in
+                print("Changing \(isChanging.location)")
+                if let player = homeVM.audioPlayer {
+                    if player.isPlaying {
+                        homeVM.stopPlayer()
+                        wasPlaying = true
+                    }
+                }
+            }
+            .onEnded { isDragging in
+                print("width: \(UIScreen.main.bounds.width * 0.9)")
+                
+                print("New place: \((isDragging.location.x / UIScreen.main.bounds.width * 0.9))")
+                if let player = homeVM.audioPlayer {
+                    let percentageToGoTo = isDragging.location.x / UIScreen.main.bounds.width * 0.9
+                    player.currentTime = percentageToGoTo * player.duration
+                    if wasPlaying {
+                        homeVM.startPlayer()
+                        wasPlaying = false
+                    }
+                }
+                
+            }
+    }
     
     var body: some View {
         VStack {
@@ -38,21 +66,23 @@ struct SoundPlayerView: View {
                     Rectangle()
                         .foregroundColor(Color.theme.customBlue)
                         .frame(width: ((UIScreen.main.bounds.width * 0.9) * homeVM.percentagePlayed))
+                        .gesture(drag)
 
                 }
+    
                 .frame(height: 5)
                 .cornerRadius(5)
                 .padding(.horizontal)
                 .overlay(alignment: .leading) {
                     Circle()
                         .foregroundColor(Color.theme.customBlue)
-                        .frame(width: 15, height: 15)
-                        .offset(x: (UIScreen.main.bounds.width * 0.9) * homeVM.percentagePlayed)
+                        .frame(width: 10, height: 10)
+                        .offset(x: (UIScreen.main.bounds.width * 0.9) * homeVM.percentagePlayed + 10)
                 }
                 HStack {
                     ClockDisplayView(time: Int(homeVM.currentTime), font: .caption)
                     Spacer()
-                    ClockDisplayView(time: Int(homeVM.duration), font: .caption)
+                    ClockDisplayView(time: Int(homeVM.audioPlayer?.duration ?? 0), font: .caption)
                 }
                 .padding(.horizontal)
                 .font(.caption)
