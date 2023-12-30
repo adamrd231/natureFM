4657ed54074300333b7db78566207e031842e81d
Adam Reed <adam.rdconcepts@gmail.com>
2023-12-30 14:33:35 -0500

    Refactor catalog view so I can see everything the view is doing at once, with extensions to clean up the main component

--- natureFM/Views/Catalog/CatalogView.swift
+++ natureFM/Views/Catalog/CatalogView.swift
@@ -1,84 +1,26 @@
 import SwiftUI
 
 struct CatalogView: View {
-    
     @ObservedObject var vm: HomeViewModel
     @ObservedObject var storeManager: StoreManager
     @ObservedObject var network: NetworkMonitor
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            // ScrollView for main container
             if network.isConnected {
-                ScrollView {
-                    if let randomSound = vm.randomSound {
-                        FeaturedImageLayoutView(
-                            sound: randomSound,
-                            storeManager: storeManager,
-                            tabSelection: $vm.tabSelection
-                        )
-                    }
-                    else {
-                        Text("Loading...")
-                    }
-                        
-                    // Sections Container
-                    VStack(alignment: .leading) {
-                        // Section One
-              
-                        HorizontalScrollView(
-                            vm: vm,
-                            soundChoice: .free,
-                            storeManager: storeManager,
-                            tabSelection: $vm.tabSelection
-                        )
-                        // Section Three
-                        HorizontalScrollView(
-                            vm: vm,
-                            soundChoice: .subscription,
-                            storeManager: storeManager,
-                            tabSelection: $vm.tabSelection
-                        )
-                        .padding(.bottom, 25)
-                    }
-                }
-                .edgesIgnoringSafeArea(.top)
+                CatalogScrollView
                 .sheet(isPresented: $vm.isViewingSongPlayer, content: {
                     SoundPlayerView()
                         .environmentObject(vm)
                 })
                 .overlay(alignment: .bottom, content: {
-                        if vm.isViewingSongPlayerTab {
-                            PlayingNowBar()
-                                .environmentObject(vm)
-                        }
+                    if vm.isViewingSongPlayerTab {
+                        PlayingNowBar()
+                            .environmentObject(vm)
                     }
-                )
+                })
             } else {
-                VStack(alignment: .center) {
-                    Image(systemName: "wifi.slash")
-                        .resizable()
-                        .frame(width: 25, height: 25)
-                    Text("No internet connection available")
-                        .bold()
-                    Text("To view sounds and download items from the library, please connect to wifi or cellular data.")
-                        .font(.caption)
-                    Button {
-                        vm.tabSelection = 2
-                    } label: {
-                        Text("Library")
-                    }
-                    .tint(Color.black.opacity(0.4))
-                    .buttonStyle(.bordered)
-       
-                }
-                .padding()
-                .background(Color.theme.backgroundColor)
-                .foregroundColor(Color.theme.titleColor)
-                .cornerRadius(10)
-                .padding()
-                .multilineTextAlignment(.center)
- 
+                NoInternetView
             }
         }
     }
@@ -91,5 +33,62 @@ struct CatalogView_Previews: PreviewProvider {
             storeManager: StoreManager(),
             network: NetworkMonitor()
         )
+    }
+}
+
+extension CatalogView {
+    var CatalogScrollView: some View {
+        ScrollView {
+            if let randomSound = vm.randomSound {
+                FeaturedImageLayoutView(
+                    sound: randomSound,
+                    storeManager: storeManager,
+                    tabSelection: $vm.tabSelection
+                )
+            }
+            // Section One
+            HorizontalScrollView(
+                vm: vm,
+                soundChoice: .free,
+                storeManager: storeManager,
+                tabSelection: $vm.tabSelection
+            )
+            // Section Three
+            HorizontalScrollView(
+                vm: vm,
+                soundChoice: .subscription,
+                storeManager: storeManager,
+                tabSelection: $vm.tabSelection
+            )
+            .padding(.bottom, 25)
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+    
+    
+    var NoInternetView: some View {
+        VStack(alignment: .center) {
+            Image(systemName: "wifi.slash")
+                .resizable()
+                .frame(width: 25, height: 25)
+            Text("No internet connection available")
+                .bold()
+            Text("To view sounds and download items from the library, please connect to wifi or cellular data.")
+                .font(.caption)
+            Button {
+                vm.tabSelection = 2
+            } label: {
+                Text("Library")
+            }
+            .tint(Color.black.opacity(0.4))
+            .buttonStyle(.bordered)
+
+        }
+        .padding()
+        .background(Color.theme.backgroundColor)
+        .foregroundColor(Color.theme.titleColor)
+        .cornerRadius(10)
+        .padding()
+        .multilineTextAlignment(.center)
     }
 }
